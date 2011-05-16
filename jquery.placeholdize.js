2f0b431b5f8c93173038d6d7677a24ad34975cad
Romain Ruetschi <romain.ruetschi@gmail.com>
2011-05-16 10:50:41 +0200

    Bumped copyright year.

--- jquery.placeholdize.js
+++ jquery.placeholdize.js
@@ -1,7 +1,7 @@
 // <![CDATA[
     
     /*
-     * Copyright (c) 2010 Romain Ruetschi <romain.ruetschi@gmail.com>
+     * Copyright (c) 2011 Romain Ruetschi <romain.ruetschi@gmail.com>
      * 
      * Permission is hereby granted, free of charge, to any person obtaining
      * a copy of this software and associated documentation files (the
